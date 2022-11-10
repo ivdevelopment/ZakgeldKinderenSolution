0910c96b54fe6e973c7ede1a12cbb780cc20f6f0
Ian Verlinden <ianverlinden1.iv@gmail.com>
2022-11-10 15:27:50 +0100

    Add project files.

---
+++ ZakgeldKinderen/Controllers/KindController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
+using ZakgeldKinderen.Models;
+
+namespace ZakgeldKinderen.Controllers
+{
+    public class KindController : Controller
+    {
+        /*private List<Kinderen> kinderen = new()
+        {
+            {new(){ Naam = "Maarten", Zakgeld = 10m }},
+            {new(){ Naam = "Jonas", Zakgeld = 7m }}
+        };*/
+        public IActionResult Index()
+        {
+            List<Kinderen> kinderen;
+            
+            return View();
+        }
+
+        /*[HttpGet]
+        public IActionResult VerwijderKind(string naam)
+        {
+            Kinderen? gelezenKind = null;
+            if (kinderen.ToString().Contains(naam))
+                kinderen.Remove(naam);
+            return gelezenPersoon;
+            return View(persoon);
+        }
+
+        [HttpPost]
+        public IActionResult Verwijderen(int id)
+        {
+            _persoonService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }*/
+    }
+}
---
+++ ZakgeldKinderen/Models/Kinderen.cs
@@ -0,0 +1,8 @@
+﻿namespace ZakgeldKinderen.Models
+{
+    public class Kinderen
+    {
+        public string Naam { get; set; }
+        public decimal Zakgeld { get; set; }
+    }
+}
